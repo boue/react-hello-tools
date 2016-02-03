fb371094b4c4360c3157fb7996d38e86ffc3d526
Germain Boue <boue.germain@gmail.com>
2016-02-02 19:37:36 -0800

    Added npm as a tool runner with two tasks dev and build

--- src/client/app/index.jsx
+++ src/client/app/index.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import {render} from 'react-dom';
 
-class App extends React.component {
+class App extends React.Component {
   render () {
     return <p> Hello React ! </p>;
   }
